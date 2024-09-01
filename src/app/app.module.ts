d81462b7d24959bce2dcaabf0176cda1bf321ecd
Simeón París <alupar9905@ieselcaminas.org>
2024-09-01 16:26:35 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GifsModule, 
+    SharedModule,
+  
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
